d31c111933a96fa0e2eaf3bac7da035ed01de17e
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-08-26 14:54:02 -0300

    [UPDATE] Cloud Spawn

--- Assets/_Project/Scripts/Cloud.cs
+++ Assets/_Project/Scripts/Cloud.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	private float _speed = 1f;
+	public float offset;
+	public float minSpeed, maxSpeed;
+	public float minSize, maxSize;
+
 	
+	void OnEnable()
+	{
+		float size = Random.Range(minSize, maxSize);
+		_speed = Random.Range(minSpeed, maxSpeed);
+		transform.localScale = Vector2.one * size;
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		// Move even if the game is paused
+		transform.Translate(Vector2.left * _speed * Time.unscaledDeltaTime);
+
+		if(IsOutOfScreen())
+		{
+			gameObject.SetActive(false);
+		}
+	}
+
+	private bool IsOutOfScreen()
+	{
+		return transform.position.x < -(CameraUtil.halfScreenWidthInWorldUnits + offset);
 	}
 }
--- Assets/_Project/Scripts/CloudController.cs
+++ Assets/_Project/Scripts/CloudController.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class CloudController : MonoBehaviour {
 
+	private ObjectPool _cloudPool;
+
+	private float _nextSpawnTime;
+	public float minSpawnTime, maxSpawnTime;
+	private float _minPosY, _maxPosY;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		_cloudPool = GetComponent<ObjectPool>();
+		_nextSpawnTime = Time.timeSinceLevelLoad + Random.Range(1f, 2f);
 		
+		_minPosY = 0f;
+		_maxPosY = CameraUtil.halfScreenHeightInWorldUnits;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		// Spawn Control
+		if(Time.timeSinceLevelLoad > _nextSpawnTime)
+		{
+			_nextSpawnTime = Time.timeSinceLevelLoad + Random.Range(minSpawnTime, maxSpawnTime);
+			Spawn();
+		}
 	}
+
+	void Spawn()
+	{
+		GameObject cloudToSpawn = _cloudPool.GetPooledObject();
+		cloudToSpawn.transform.position = new Vector3(transform.position.x, Random.Range(_minPosY, _maxPosY));
+		cloudToSpawn.transform.rotation = Quaternion.identity;
+
+		cloudToSpawn.SetActive(true);
+	}
+
 }
