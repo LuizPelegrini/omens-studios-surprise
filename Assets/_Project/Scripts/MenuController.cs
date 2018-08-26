147c2a14d05ff4a04f31578045c527486088f857
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-08-26 14:17:05 -0300

    [ADD] Menu screen

---
+++ Assets/_Project/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	public Button playButton;
+	public Button instructionsButton;
+	public Button closePanelButton;
+
+	public GameObject instructionsPanel;
+
+	private const string _sceneLevelName = "Level";
+
+	// Use this for initialization
+	void Start () 
+	{
+		playButton.onClick.AddListener(Play);
+		
+		instructionsButton.onClick.AddListener(() => {
+			instructionsPanel.SetActive(true);
+		});	
+		
+		closePanelButton.onClick.AddListener(() => {
+			instructionsPanel.SetActive(false);
+		});
+	}
+
+	void Play()
+	{
+		SceneManager.LoadScene(_sceneLevelName);
+	}
+}
