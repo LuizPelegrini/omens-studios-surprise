ca9876059c75e25e859a6f6ffea6eba1351071f0
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-08-25 21:09:05 -0300

    [ADD] Jump and walking animation

---
+++ Assets/_Project/Scripts/AnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+	private const string animatorTransition = "movingHorizontally";
+	Player _player;
+	Animator _anim;
+
+	void Start()
+	{
+		_player = GetComponentInParent<Player>();
+		_anim = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		if(Input.GetAxisRaw("Horizontal") != 0)
+			_anim.SetBool(animatorTransition, true);
+		else
+			_anim.SetBool(animatorTransition, false);
+
+		// _anim.SetBool("jump", Mathf.Abs(_player.velocity.y) >= 2);
+		_anim.SetBool("jump", _player.movementController.collisionInfo.below);
+		_anim.SetFloat("velocityY", _player.velocity.y);
+	}
+
+}
--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -3,10 +3,11 @@
 [RequireComponent(typeof(MovementController))]
 public class Player : MonoBehaviour {
 
-	private Vector2 _velocity;
-	private MovementController _movementController;
+	[HideInInspector] public Vector2 velocity;
+	[HideInInspector] public MovementController movementController;
 	private float changingDirectionGrounded = .2f;
 	private float changingDirectionOnAir = .1f;
+	private bool _isFacingRight = true;
 
 	[SerializeField] private float _horizontalSpeed = 10f;
 	[SerializeField] private float _gravity = -20f;
@@ -14,29 +15,49 @@ public class Player : MonoBehaviour {
 
 	void Start()
 	{
-		_movementController = GetComponent<MovementController>();
+		movementController = GetComponent<MovementController>();
 	}
 
 	void Update()
 	{
 		// prevent accumulating gravity across multiple frames, if the player is colliding vertically
-		if(_movementController.collisionInfo.above || _movementController.collisionInfo.below)
-			_velocity.y = 0f;
+		if(movementController.collisionInfo.above || movementController.collisionInfo.below)
+			velocity.y = 0f;
 
 		// If the player presses the Jump button and there is a collision below it, so he can jump
-		if(Input.GetKeyDown(KeyCode.Space) && _movementController.collisionInfo.below)
-			_velocity.y = _jumpForce;
+		if(Input.GetKeyDown(KeyCode.Space) && movementController.collisionInfo.below)
+			velocity.y = _jumpForce;
 
 		// Apply gravity per-frame portion
-		_velocity.y += _gravity * Time.deltaTime;
+		velocity.y += _gravity * Time.deltaTime;
 
 		// Target horizontal input
 		float targetVeloctityX = Input.GetAxisRaw("Horizontal") * _horizontalSpeed;
 
-		float changingDirection = _movementController.collisionInfo.below ? changingDirectionGrounded : changingDirectionOnAir;
-		_velocity.x = Mathf.Lerp(_velocity.x, targetVeloctityX, changingDirection);
+		float changingDirection = movementController.collisionInfo.below ? changingDirectionGrounded : changingDirectionOnAir;
+		velocity.x = Mathf.Lerp(velocity.x, targetVeloctityX, changingDirection);
+
+		CheckDirection();
 
 		// Move the player by certain amount (based on velocity)
-		_movementController.Move(_velocity * Time.deltaTime);
+		movementController.Move(velocity * Time.deltaTime);
+	}
+
+	void CheckDirection()
+	{
+		bool movingRight = Mathf.Sign(velocity.x) >= 1 ? true : false;
+
+		if(_isFacingRight && !movingRight)
+		{
+			transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+			_isFacingRight = !_isFacingRight;
+		}
+
+		if(!_isFacingRight && movingRight)
+		{
+			transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+			_isFacingRight = !_isFacingRight;
+		}
+
 	}
 }
